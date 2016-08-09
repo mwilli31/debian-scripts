33e7c3ad8e41200a036ab546f7bfc32fba78d224
michaelciancimino <michaelciancimino@gmail.com>
2016-08-09 10:28:38 -0400

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -37,16 +37,16 @@ do
 done
 
 #provision flowthings
-./predix/debian-scripts/provision-scripts/provision_flowthings.sh -u $U_ARG -t $T_ARG -i $I_ARG
+/predix/debian-scripts/provision-scripts/provision_flowthings.sh -u $U_ARG -t $T_ARG -i $I_ARG
 
 #provision kit specific drivers
-./predix/debian-scripts/provision-scripts/provision_drivers.sh -k $K_ARG
+/predix/debian-scripts/provision-scripts/provision_drivers.sh -k $K_ARG
 
 #provision predix machine
-./predix/debian-scripts/provision-scripts/provision_predix_machine.sh
+/predix/debian-scripts/provision-scripts/provision_predix_machine.sh
 
 #provision local asset
-./predix/debian-scripts/provision-scripts/provision_local_asset.sh
+/predix/debian-scripts/provision-scripts/provision_local_asset.sh
 
 echo "Restarting Services"
 systemctl daemon-reload
