1f6bc67da4b4960bf10fc5e4e12077533318b271
michaelciancimino <michaelciancimino@gmail.com>
2016-08-09 14:41:30 -0400

    Update provision_predix_machine.sh

--- provision-scripts/provision_predix_machine.sh
+++ provision-scripts/provision_predix_machine.sh
@@ -8,7 +8,7 @@ git fetch --all
 git reset --hard origin/master
 git pull origin master
 echo "Predix Machine updated"
-./predix/debian-scripts/provision-scripts/pm_service_setup.sh
+/predix/debian-scripts/provision-scripts/pm_service_setup.sh
 echo "Predix Machine Service Updated"
 systemctl start predix-machine
 systemctl enable predix-machine
