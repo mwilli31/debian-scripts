3ce11a5445b2313db96ec5b363162c572b5c41ac
michaelciancimino <michaelciancimino@gmail.com>
2016-08-15 14:07:54 -0400

    Update hostnameServiceSetup.sh

--- adhoc-scripts/hostnameServiceSetup.sh
+++ adhoc-scripts/hostnameServiceSetup.sh
@@ -4,7 +4,7 @@ echo 'Description=Hostname Setup' >> /etc/systemd/system/hostname-setup.service
 echo 'After=network.target' >> /etc/systemd/system/hostname-setup.service
 echo '' >> /etc/systemd/system/hostname-setup.service
 echo '[Service]' >> /etc/systemd/system/hostname-setup.service
-echo 'ExecStart=/predix/debian-scripts/adhoc-scripts/hostnameSetup.sh' >> /etc/systemd/system/hostname-setup.service
+echo 'ExecStart=/bin/bash /predix/debian-scripts/adhoc-scripts/hostnameSetup.sh' >> /etc/systemd/system/hostname-setup.service
 echo '' >> /etc/systemd/system/hostname-setup.service
 echo '[Install]' >> /etc/systemd/system/hostname-setup.service
 echo 'WantedBy=multi-user.target' >> /etc/systemd/system/hostname-setup.service
