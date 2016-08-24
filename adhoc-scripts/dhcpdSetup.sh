cbffd1f2919861708f624e963f53516fa6ec2604
mwilli31 <mjwilli31@gmail.com>
2016-08-24 16:35:35 -0700

    removing router, ad-hoc network unusable with it

--- adhoc-scripts/dhcpdSetup.sh
+++ adhoc-scripts/dhcpdSetup.sh
@@ -6,5 +6,4 @@ echo 'authoritative;' >> /etc/dhcp/dhcpd.conf
 echo 'log-facility local7;' >> /etc/dhcp/dhcpd.conf
 echo 'subnet 192.168.122.0 netmask 255.255.255.0 {' >> /etc/dhcp/dhcpd.conf
 echo '	range 192.168.122.5 192.168.122.150;' >> /etc/dhcp/dhcpd.conf
-echo '  option routers 192.168.122.1;' >> /etc/dhcp/dhcpd.conf
 echo '}' >> /etc/dhcp/dhcpd.conf
