025e066007692f11466343346ade0c95dae7d302
mwilli31 <mjwilli31@gmail.com>
2016-08-24 15:19:22 -0700

    Update dhcpdSetup.sh

--- adhoc-scripts/dhcpdSetup.sh
+++ adhoc-scripts/dhcpdSetup.sh
@@ -4,6 +4,6 @@ echo 'default-lease-time 600;' >> /etc/dhcp/dhcpd.conf
 echo 'max-lease-time 7200;' >> /etc/dhcp/dhcpd.conf
 echo 'authoritative;' >> /etc/dhcp/dhcpd.conf
 echo 'log-facility local7;' >> /etc/dhcp/dhcpd.conf
-echo 'subnet 192.168.1.0 netmask 255.255.255.0 {' >> /etc/dhcp/dhcpd.conf
-echo '	range 192.168.1.5 192.168.1.150;' >> /etc/dhcp/dhcpd.conf
+echo 'subnet 192.168.122.0 netmask 255.255.255.0 {' >> /etc/dhcp/dhcpd.conf
+echo '	range 192.168.122.5 192.168.1.150;' >> /etc/dhcp/dhcpd.conf
 echo '}' >> /etc/dhcp/dhcpd.conf
