4dd1ab6683e15887cf7cb59081d12fcc6ff96e48
mwilli31 <mjwilli31@gmail.com>
2016-08-24 17:10:14 -0700

    back to interim we go!

--- adhoc-scripts/dhcpdSetup.sh
+++ adhoc-scripts/dhcpdSetup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo 'ddns-update-style standard;' > /etc/dhcp/dhcpd.conf
+echo 'ddns-update-style interim;' > /etc/dhcp/dhcpd.conf
 echo 'default-lease-time 600;' >> /etc/dhcp/dhcpd.conf
 echo 'max-lease-time 7200;' >> /etc/dhcp/dhcpd.conf
 echo 'authoritative;' >> /etc/dhcp/dhcpd.conf
 echo 'log-facility local7;' >> /etc/dhcp/dhcpd.conf
 echo 'subnet 192.168.122.0 netmask 255.255.255.0 {' >> /etc/dhcp/dhcpd.conf
-echo '	range 192.168.122.5 192.168.122.150;' >> /etc/dhcp/dhcpd.conf
+echo '	range 192.168.122.10 192.168.122.20;' >> /etc/dhcp/dhcpd.conf
 echo '}' >> /etc/dhcp/dhcpd.conf
